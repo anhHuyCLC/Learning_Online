706a1e71b031473e16e2df9df640c1ceb9f4f1bf
anhHuyCLC <huytrinh19604@gmail.com>
2026-04-17 18:14:12 +0700

    fix api

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = (import.meta.env.VITE_API_URL || "http://localhost:3000") + "/api";
+const API_URL = (import.meta.env.VITE_API_URL || "https://gout-atop-protract.ngrok-free.dev") + "/api";
 
 const apiClient = axios.create({
   baseURL: API_URL,
